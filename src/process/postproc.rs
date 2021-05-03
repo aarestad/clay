492f0f169f99f6e86ad443af8dc720d789a90f9e
Peter Aarestad <peter.aarestad@tempus.com>
2021-05-03 16:57:19 -0500

    spelling

--- src/process/postproc.rs
+++ src/process/postproc.rs
@@ -1,14 +1,14 @@
 pub use crate::core::process::{Postproc, PostprocBuilder, PostprocCollector};
 use crate::filter::{Filter, IdentityFilter};
 
-/// Creates postrocessor with already included device source from `clay` and `clay-core`.
+/// Creates postprocessor with already included device source from `clay` and `clay-core`.
 pub fn create_postproc<F: Filter>() -> PostprocCollector<F> {
     let mut collector = crate::core::process::create_postproc::<F>();
     collector.add_hook(crate::source());
     collector
 }
 
-/// Creates postrocessor with identity filter.
+/// Creates postprocessor with identity filter.
 pub fn create_default_postproc() -> PostprocCollector<IdentityFilter> {
     create_postproc::<IdentityFilter>()
 }
